ae3b99e95d579378c27a0468a1d3457b56a49d6a
Carl Wong <carl@winsoft.ca>
2020-01-07 13:27:07 -0500

    Update index.js
    Added CORS options to whitelist origins.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
-const dbConfig = require('./connection');
-
 const express = require('express');
+
+// CORS setup
 const cors = require('cors');
+const corsWhitelist = [
+	'http://localhost:4200',
+	'https://decktech.narl.life'
+];
+const corsOptions = {
+	origin: function(origin, callback) {
+		if (corsWhitelist.indexOf(origin) !== -1) {
+			callback(null, true);
+		} else {
+			callback(new Error('Not allowed by CORS'));
+		}
+	}
+}
+
 const bodyParser = require('body-parser');
+
+// database details
 const mysql = require('mysql');
-
-const apiOracleCards = require('./oracle-cards');
-const apiProfiles = require('./profiles');
-const apiTags = require('./tags');
-const apiCardTagLinks = require('./card-tag-links');
-
+const dbConfig = require('./connection');
 const pool = mysql.createPool({
 	connectionLimit: 10,
 	host: dbConfig.host,
@@ -18,10 +29,16 @@ const pool = mysql.createPool({
 	database: dbConfig.database,
 });
 
+// API controllers
+const apiOracleCards = require('./oracle-cards');
+const apiProfiles = require('./profiles');
+const apiTags = require('./tags');
+const apiCardTagLinks = require('./card-tag-links');
+
 const port = process.env.PORT || 3000;
 
 const app = express()
-	.use(cors())
+	.use(cors(corsOptions))
 	.use(bodyParser.json())
 	.use(apiOracleCards(pool))
 	.use(apiProfiles(pool))
