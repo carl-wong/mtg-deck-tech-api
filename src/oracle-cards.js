9c8de1c993f34dd9df7ad3b13792ad307ec46aa3
Carl Wong <carl@winsoft.ca>
2020-01-28 22:36:54 -0500

    Attempt to fetch cards via POST

--- src/oracle-cards.js
+++ src/oracle-cards.js
@@ -88,6 +88,39 @@ function createRouter(pool) {
 			);
 		});
 	});
+
+	router.post('/OracleCards', (req, res, next) => {
+		let whereClauses = [];
+
+		// req.body.cards: string[]
+		if (req.body.cards) {
+			req.body.cards.forEach(card => {
+				whereClauses.push(`name=${connection.escape(card)}`);
+			});
+		}
+
+		if (whereClauses.length > 0) {
+			const query = 'SELECT * FROM OracleCard WHERE ' + whereClauses.join(' OR ');
+			pool.getConnection(function(err, connection) {
+				connection.query(
+					query,
+					(error, results) => {
+						connection.release();
+
+						if (error) {
+							console.log(error);
+							res.status(500).json(null);
+						} else {
+							res.status(200).json(results);
+						}
+					}
+				);
+			});
+		} else {
+			res.status(500).json(null);
+		}
+	});
+
 	return router;
 }
 
